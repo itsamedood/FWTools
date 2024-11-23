1271172983a7ae73a43261ba7aa523d92afe2e1b
itsamedood <itsamedood@gmail.com>
2024-11-23 16:56:49 -0500

    Tried to add logo to main frame

--- src/frames/mainFrame.py
+++ src/frames/mainFrame.py
@@ -1,3 +1,4 @@
+from PIL import Image, ImageTk
 from tkinter import Button, Frame, Label
 
 
@@ -10,7 +11,14 @@ class MainFrame(Frame):
     self.columnconfigure(1, weight=1)
 
     # Create elements.
-    title_label = Label(self, text="FWTools - itsamedood (C) 2024", pady=20)
+    oplogo = Image.open("assets/logos/FWToolslogo.png")
+    oplogo = oplogo.resize((200, 150))
+    logo = ImageTk.PhotoImage(oplogo)
+
+
+    logo_label = Label(self, image=logo)
+    # title_label = Label(self, text="FWTools - itsamedood (C) 2024", pady=10, font=("Arial", 25))
 
     # Add elements.
-    title_label.grid(row=0, column=0, columnspan=2)
+    logo_label.grid(row=0, column=0, columnspan=2)
+    # title_label.grid(row=20, column=0, columnspan=2)
