a34686d624a6c5165fb1de0ecfdd0940307ba4c0
itsamedood <itsamedood@gmail.com>
2024-11-23 16:34:38 -0500

    Added title for main frame

--- src/frames/mainFrame.py
+++ src/frames/mainFrame.py
@@ -1,4 +1,4 @@
-from tkinter import Frame, Label
+from tkinter import Button, Frame, Label
 
 
 class MainFrame(Frame):
@@ -10,7 +10,7 @@ class MainFrame(Frame):
     self.columnconfigure(1, weight=1)
 
     # Create elements.
-    ...
+    title_label = Label(self, text="FWTools - itsamedood (C) 2024", pady=20)
 
     # Add elements.
-    ...
+    title_label.grid(row=0, column=0, columnspan=2)
